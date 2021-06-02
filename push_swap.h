cde22199939814936dc09f60f5610ad1aa5142a9
amarini- <amarini-@42.fr>
2021-06-02 18:50:32 +0200

    started something, need to follow the baby steps

---
+++ push_swap.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/02 15:11:18 by amarini-          #+#    #+#             */
+/*   Updated: 2021/06/02 15:47:23 by amarini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft/libft.h"
+
+typedef struct s_list
+{
+	int				value;
+	struct s_list	*next;
+}				t_list;
+
+#endif
